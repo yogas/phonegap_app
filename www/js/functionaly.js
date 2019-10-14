89b8240602d458b283e6a58c2f05f7464ad7631e
Alexey Svechnikov <ny_18@list.ru>
2019-10-14 15:21:02 +0600

    session storage in db feature and update jquery mobile

--- www/js/functionaly.js
+++ www/js/functionaly.js
@@ -23,6 +23,11 @@ function getDataFromServer() {
 }
 
 
+function getSessionID() {
+    return window.localStorage["acc_sessionID"];
+}
+
+
 function checkSession() {
 	ajax("check_server_session");
 }
@@ -33,52 +38,81 @@ function ajax_response_ctrl(g, res) {
 		case 'auth_user':
 			if (res.token) {
             	//document.getElementById("user_pass").style.border = "solid 1px red";
-                localStorage.setItem("acc_sessionID", 1);
-                //window.localStorage["user_email"] = res.data;
+                window.localStorage["acc_sessionID"] = 1;
+                window.localStorage["user_email"] = res.data;
                 $.mobile.changePage('#devices');
                 getDataFromServer();
 			} else {
-                localStorage.setItem("acc_sessionID", 0);
-                //window.localStorage["user_email"] = null;
+                window.localStorage["acc_sessionID"] = 0;
+                window.localStorage["user_email"] = null;
                 $.mobile.changePage('#auth');
 			}
-            //console.log(window.localStorage["acc_sessionID"]);
-            //console.log(window.localStorage["user_email"]);
 		break;
         case 'get_devices_list':
             if (res.data) {
-                var html_source = '<ul>';
+
+                var html_source = '';
+                //var html_source = '<ul>';
 
                 for (var t=0; t!=res.data.length; t++) {
+                    /*
                     html_source += '<li><b>Device ID:</b> '+ res.data[t]['id'] +'</li>';
                     html_source += '<li><b>Device Name:</b> '+ res.data[t]['name'] +'</li>';
                     html_source += '<li><b>Device Type:</b> '+ res.data[t]['type'] +'</li>';
                     html_source += '<li><b>Device Object:</b> '+ res.data[t]['object'] +'</li>';
                     html_source += '<li><b>Device Parent Object:</b> '+ res.data[t]['parent_object'] +'</li>';
                     html_source += '<li><b>Device Identifier:</b> '+ res.data[t]['identifier'] +'</li>';
+                    */
+
+                    html_source += '<a href="#" style="margin-bottom:15px;"><div class="devoce-smart-block">';
+                    html_source += '<div class="row" style="padding-top:15px;">';
+                    html_source += '<div class="device-param-label">Компания</div>';
+                    html_source += '<div class="device-param-value">ООО "Трейд Холдинг"</div>';
+                    html_source += '</div>';
+                    html_source += '<div class="clr"></div>';
+                    html_source += '<div class="row">';
+                    html_source += '<div class="device-param-label">Объект</div>';
+                    html_source += '<div class="device-param-value">'+ res.data[t]['object'] +'</div>';
+                    html_source += '</div>';
+                    html_source += '<div class="clr"></div>';
+                    html_source += '<div class="row">';
+                    html_source += '<div class="device-param-label">Резервуар</div>';
+                    html_source += '<div class="device-param-value">'+ res.data[t]['name'] +'</div>';
+                    html_source += '</div>';
+                    html_source += '<div class="clr"></div>';
+                    html_source += '<div class="row-blocks">';
+                    html_source += '<div class="param-block" style="margin-right:35px;">';
+                    html_source += '<span class="temp">Температура, С</span>';
+                    html_source += '30';
+                    html_source += '</div>';
+                    html_source += '<div class="param-block" style="margin-right:35px;">';
+                    html_source += '<span class="volume">Объем, м<sup>3</sup></span>';
+                    html_source += '234,6';
+                    html_source += '</div>';
+                    html_source += '<div class="param-block">';
+                    html_source += '<span class="alert">Авария</span>';
+                    html_source += 'нет';
+                    html_source += '</div>';
+                    html_source += '</div>';
+                    html_source += '</div></a>';
                 }
 
-                html_source += '</ul>';
+                //html_source += '</ul>';
 
                 $("#devices-page-content").html("");
                 $("#devices-page-content").html(html_source);
 
-                //console.log($("#index-page-content"));
             } else {
                 //console.log();
             }
         break;
 		case 'check_server_session':
             if (null !== res.data) {
-                localStorage.setItem("acc_sessionID", 1);
-                //console.log(window.localStorage["acc_sessionID"]);
+                window.localStorage["acc_sessionID"] = 1;
             } else {
-                localStorage.setItem("acc_sessionID", 0);
-                //localStorage["user_email"] = null;
-                $.mobile.changePage('#auth');
+                window.localStorage["acc_sessionID"] =  0;
+                window.localStorage["user_email"] = null;
             }
-            //console.log(window.localStorage["acc_sessionID"]);
-            //console.log(window.localStorage["user_email"]);
 		break;
 	}
 }
@@ -93,9 +127,9 @@ function ajax(g, oid) {
         window.localStorage["user_email"] = user_email;
         xmlhttp.open('GET', 'http://dev.hashing24.sale/main/login?&fields[login]='+user_email+'&fields[password]='+user_pass, true);
 	} else if (g == "check_server_session") {
-        xmlhttp.open('GET', 'http://dev.hashing24.sale/main/checkSession', true);
+        xmlhttp.open('GET', 'http://dev.hashing24.sale/main/checkSession?&identifer='+window.localStorage["user_email"], true);
 	} else if (g == "get_devices_list") {
-        xmlhttp.open('GET', 'http://dev.hashing24.sale/main/getDevices', true);
+        xmlhttp.open('GET', 'http://dev.hashing24.sale/main/getDevices?&identifer='+window.localStorage["user_email"], true);
 	} else {
         //xmlhttp.open('GET', 'http://super.aspen.ru/mobile_ajax_files/test.php?operation='+g+'&eri='+app_ex_run_id+'&rndtik='+randomNum, true);
 	}
