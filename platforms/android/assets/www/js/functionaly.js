cb12851ceec8c54d5ddb663de137260a3b8f5d1f
Alexey Svechnikov <ny_18@list.ru>
2019-10-15 22:05:03 +0600

    feat pack #3

--- platforms/android/assets/www/js/functionaly.js
+++ platforms/android/assets/www/js/functionaly.js
@@ -1,5 +1,10 @@
 
 
+function numberWithSpaces(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
+
+
 function getXmlHttp() {
 	var xmlhttp;
 	try {
@@ -22,7 +27,19 @@ function getDataFromServer() {
 	ajax("get_devices_list");
 }
 
+
+function getMySessionID() {
+    //console.log(window.localStorage["acc_sessionID"]);
+    if (parseInt(window.localStorage["acc_sessionID"]) == 1) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
 function getDeviceDetailData(oid) {
+    ajax("check_server_session");
     ajax("get_device_detail", oid);
     $.mobile.changePage('#device-detail');
 }
@@ -37,7 +54,9 @@ function ajax_response_ctrl(g, res) {
 	switch(g) {
 		case 'auth_user':
             //alert(6576765756756);
+            //console.log(res);
 			if (res.token) {
+                $("#auth-error-msg").html("");
                 //alert('xxxxxxxxxxxxxxxxxxxxx');
             	//document.getElementById("user_pass").style.border = "solid 1px red";
                 window.localStorage["acc_sessionID"] = 1;
@@ -45,10 +64,12 @@ function ajax_response_ctrl(g, res) {
                 $.mobile.changePage('#devices');
                 getDataFromServer();
 			} else {
+			    //console.log($("#auth-error-msg"));
                 //alert('yyyyyyyyyyyyyyyyyyyyyy');
                 window.localStorage["acc_sessionID"] = 0;
                 window.localStorage["user_email"] = null;
                 $.mobile.changePage('#auth');
+                $("#auth-error-msg").html(res.error_msg);
 			}
 		break;
         case 'get_devices_list':
@@ -108,6 +129,7 @@ function ajax_response_ctrl(g, res) {
             } else {
                 window.localStorage["acc_sessionID"] =  0;
                 window.localStorage["user_email"] = null;
+                $.mobile.changePage('#auth');
             }
 		break;
         case 'get_device_detail':
@@ -117,23 +139,57 @@ function ajax_response_ctrl(g, res) {
 
                 var tens = new Array();
                 var tempos = new Array();
+                var tempos_codes = new Array();
+                var tens_codes = new Array();
+
+                var tens_manuals = new Array();
+                var tens_alarm = new Array();
+                var tens_manuals_finish = new Array();
+                var tens_alarm_finish = new Array();
+
                 var gtn = 1;
                 var gtn2 = 1;
+                var gtn3 = 1;
+                var gtn4 = 1;
                 var tens_finish = new Array();
                 var tempos_finish = new Array();
                 var diam = 0;
                 var dh = 0;
                 var h = 0;
                 var tos = 0;
+                var sn = 0;
+
+                //var alarmTn = 0;
 
                 for (var n=0; n!=res.data.parameters.length; n++) {
                     var code = String(res.data.parameters[n].code);
+                    if (code == "Tn"+gtn3+"_Manual") {
+                        //console.log(code);
+                        //console.log(parseInt(res.data.parameters[n].value));
+                        if (parseInt(res.data.parameters[n].value) == 1 || res.data.parameters[n].value == "1") {
+                            tens_manuals[n] = 'P';
+                        } else {
+                            tens_manuals[n] = 'A';
+                        }
+                        gtn3++;
+                    }
+                    if (code == "Tn"+gtn4+"_Fau") {
+                        if (parseInt(res.data.parameters[n].value) == 1 || res.data.parameters[n].value == "1") {
+                            tens_alarm[n] = '#BE3D3D';
+                        } else {
+                            tens_alarm[n] = '#3DBE52';
+                        }
+                        gtn4++;
+                    }
+
                     if (code == "Tn"+gtn) {
-                        tens[n] = code;
+                        tens[n] = res.data.parameters[n].value;
+                        tens_codes[n] = code;
                         gtn++;
                     }
                     if (code == "T"+gtn2) {
-                        tempos[n] = code;
+                        tempos[n] = res.data.parameters[n].value;
+                        tempos_codes[n] = code;
                         gtn2++;
                     }
                     if (code == "Diam") {
@@ -145,10 +201,37 @@ function ajax_response_ctrl(g, res) {
                     if (code == "H") {
                         h = res.data.parameters[n].value;
                     }
-                    if (tos == "T_OS") {
-                        h = res.data.parameters[n].value;
+                    if (code == "T_OS") {
+                        tos = res.data.parameters[n].value;
+                    }
+                    if (code == "Sn") {
+                        sn = res.data.parameters[n].value;
                     }
                 }
+
+                //console.log(tens_manuals);
+
+                var t = 0;
+                for (var n=0; n!=tens_manuals.length; n++) {
+                    if (undefined !== tens_manuals[n]) {
+                        tens_manuals_finish[t] = tens_manuals[n];
+                        t++;
+                    } else {
+                        continue;
+                    }
+                }
+                var t = 0;
+                for (var n=0; n!=tens_alarm.length; n++) {
+                    if (undefined !== tens_alarm[n]) {
+                        tens_alarm_finish[t] = tens_alarm[n];
+                        t++;
+                    } else {
+                        continue;
+                    }
+                }
+
+                //console.log(tens_manuals_finish);
+                //console.log(tens_alarm_finish);
 
                 var t = 0;
                 for (var n=0; n!=tens.length; n++) {
@@ -181,17 +264,22 @@ function ajax_response_ctrl(g, res) {
                 var lineheight_temp_block = device_wf_h / tempos_finish.length;
                 var bgs = ["#BE3D3D", "#3DBE52", "#3D88BE"];
 
+                var dtn2 = 0;
                 for(var dtn=0; dtn!=tempos_finish.length; dtn++) {
+                    //console.log(tens_manuals_finish[dtn]);
+                    //console.log(tens_alarm_finish[dtn]);
+
                     if (dtn == 0) {
                         var lineheight = lineheight_temp_block / 4;
                     } else {
                         var lineheight = ((lineheight_temp_block * dtn) + (lineheight_temp_block / 2)) - (lineheight_temp_block / 4);
                     }
-                    deviceDt += '<span class="device-dt" style="top:'+lineheight+'px; background-color:'+bgs[dtn]+'; left:-30px;">'+tempos_finish[dtn]+'</span>';
-                    deviceDt += '<span style="/*top:'+lineheight+'px; */display:block; position:relative; z-index:33; width:55px; float:right; right:-30px; clear:both;">';
+                    deviceDt += '<span class="device-dt" style="top:'+lineheight+'px; background-color:'+bgs[dtn]+'; left:-30px; line-height:2;">+'+tempos_finish[dtn]+' &#8451;</span>';
+                    deviceDt += '<span style="top:'+lineheight+'px; display:block; position:relative; z-index:48; width:55px; float:right; right:-14px; clear:both;">';
 
                     for (var tnn=0; tnn!=ten_of_section; tnn++) {
-                        deviceDt += '<span class="device-tn" style="">'+tempos_finish[dtn]+'</span>';
+                        deviceDt += '<span class="device-tn" style="background-color:'+tens_alarm_finish[dtn2]+';">'+tens_manuals_finish[dtn2]+'</span>';
+                        dtn2++;
                     }
 
                     deviceDt += '</span>';
@@ -199,11 +287,11 @@ function ajax_response_ctrl(g, res) {
 
                 var one_percent_H = Math.floor((h / 100) * 10000) / 10000;
                 var percents_L = Math.floor((dh / one_percent_H) * 100) / 100;
-                var vodichkaHeight = Math.ceil(6 * percents_L);
-                var vodichkaTop = 600 - vodichkaHeight;
+                var vodichkaHeight = Math.ceil(5.5 * percents_L);
+                //var vodichkaTop = 550 - vodichkaHeight;
 
                 //$("#device-work-frame").html(deviceDt);
-                detail_page_html_source += '<div class="device-schema-block">\n' +
+                detail_page_html_source += '<div class="sn-block">№ '+sn+'</div><div class="device-schema-block">\n' +
                     '                        <div class="hight-level-brd">\n' +
                     '                            <span class="hight-lavel-block">ПЕРЕЛИВ</span>\n' +
                     '                        </div>\n' +
@@ -219,7 +307,7 @@ function ajax_response_ctrl(g, res) {
                     '                        <div class="low-level-brd">\n' +
                     '                            <span class="low-lavel-block">НЕВЫБИРАЕМЫЙ ОСТАТОК</span>\n' +
                     '                        </div>\n' +
-                    '                    <div class="vodichka" style="height:'+vodichkaHeight+'px; margin-top:-'+(vodichkaHeight - 10)+'px;"></div></div>';
+                    '                    <div class="vodichka" style="height:'+vodichkaHeight+'px; margin-top:-'+(vodichkaHeight - 10)+'px;"><div style="color:#000000; text-align:left; margin-left:15px; padding-top:5px;">'+dh+' м / '+h+' м</div><div class="vodichka-text">' + numberWithSpaces(Math.floor(l * 10) / 10) + ' л</div></div></div>';
 
 
                 detail_page_html_source += '<ul>';
