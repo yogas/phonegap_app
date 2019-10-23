dc496730b508bf7abaf00469b80260f08560fa74
Alexey Svechnikov <ny_18@list.ru>
2019-10-24 01:10:54 +0600

    push fix #2 23.10.2019

---
+++ platforms/browser/www/js/index.js
@@ -0,0 +1,138 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*
+function urlB64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+    console.log(base64);
+    const rawData = window.atob(base64);
+    console.log(rawData);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
+
+*/
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        //console.log(app);
+        app.receivedEvent('index');
+        /*
+        navigator.notification.alert(
+            'You are the winner!',  // message
+            //alertDismissed,         // callback
+            'Game Over',            // title
+            'Done'                  // buttonName
+        );
+        */
+        app.setupPush();
+    },
+
+    setupPush: function() {
+        //alert('calling push init');
+        var push = PushNotification.init({
+            "android": {
+                "senderID": "405571286464"
+            },
+            "browser": {
+                "pushServiceURL": 'http://push.wr'
+            },
+            "ios": {
+                "sound": true,
+                "vibration": true,
+                "badge": true
+            },
+            "windows": {}
+        });
+        //alert('after init');
+
+        //console.log(push);
+
+        push.on('registration', function(data) {
+            alert('registration event: ' + data.registrationId);
+
+            var oldRegId = localStorage.getItem('registrationId');
+            if (oldRegId !== data.registrationId) {
+                // Save new registration ID
+                localStorage.setItem('registrationId', data.registrationId);
+                // Post registrationId to your app server as the value has changed
+                checkDevices();
+            }
+
+            //var parentElement = document.getElementById('registration');
+            //var listeningElement = parentElement.querySelector('.waiting');
+            //var receivedElement = parentElement.querySelector('.received');
+
+            //listeningElement.setAttribute('style', 'display:none;');
+            //receivedElement.setAttribute('style', 'display:block;');
+
+        });
+
+        push.on('error', function(e) {
+            alert("push error = " + e.message);
+        });
+
+        push.on('notification', function(data) {
+            alert('notification event');
+            navigator.notification.alert(
+                data.message,         // message
+                null,                 // callback
+                data.title,           // title
+                'Ok'                  // buttonName
+            );
+        });
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        //var listeningElement = parentElement.querySelector('.listening');
+        //var receivedElement = parentElement.querySelector('.received');
+
+        //listeningElement.setAttribute('style', 'display:none;');
+        //receivedElement.setAttribute('style', 'display:block;');
+
+        //console.log('Received Event: ' + id);
+        //getDataFromServer();
+        //checkSession();
+    }
+};
+
+app.initialize();
